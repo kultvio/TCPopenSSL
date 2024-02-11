99119ddea1aea76c1d5fac4a3f575787e784a5b3
kultvio <96407479+kultvio@users.noreply.github.com>
2024-02-11 14:04:34 +1000

    CreatedProject

---
+++ Server/Server.cpp
@@ -0,0 +1,18 @@
+﻿// Server.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#include "WinSock2.h"
+int main()
+{
+    WSADATA WSdata;
+    if (WSAStartup(MAKEWORD(2, 2), &WSdata) == SOCKET_ERROR)
+    {
+        std::cout << "Couldn't init WSA!";
+        return 1;
+    }
+    
+    return 0;
+}
+
