107b8ac9c05fbd04f6902a037c120350161a7624
kultvio <96407479+kultvio@users.noreply.github.com>
2024-02-12 22:58:09 +1000

    12.02.24

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,7 +1,37 @@
-﻿
-#include <iostream>
-
+﻿#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#include "WinSock2.h"
+#pragma warning(disable:4996)
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string ipaddress = "127.0.0.1";
+    int port = 8888;
+    WSADATA WSdata;
+    if (WSAStartup(MAKEWORD(2, 1), &WSdata) == SOCKET_ERROR)
+    {
+        std::cout << "Couldn't init WSA!";
+        return 1;
+    }
+
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr(ipaddress.c_str());
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+
+    SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+    if(connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0)
+    {
+        std::cout << "Error: failed connect to server! \n";
+        return 1;
+    }
+
+    std::cout << "Connect success \n";
+    char msg[256];
+    recv(Connection, msg, sizeof(msg), NULL);
+    std::cout << msg;
+    
+
+    system("pause");
+    return 0;
 }
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -4,15 +4,44 @@
 #include <iostream>
 #pragma comment(lib, "ws2_32.lib")
 #include "WinSock2.h"
+#pragma warning(disable:4996)
 int main()
 {
+    std::string ipaddress = "127.0.0.1";
+    int port = 8888;
     WSADATA WSdata;
-    if (WSAStartup(MAKEWORD(2, 2), &WSdata) == SOCKET_ERROR)
+    if (WSAStartup(MAKEWORD(2, 1), &WSdata) == SOCKET_ERROR)
     {
         std::cout << "Couldn't init WSA!";
         return 1;
     }
-    
+
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr(ipaddress.c_str());
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+
+    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+    bind(sListen, (SOCKADDR*)&addr, sizeofaddr);
+    listen(sListen, SOMAXCONN);
+
+    SOCKET newConnetion;
+    newConnetion = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+    if (newConnetion == 0)
+    {
+        std::cout << "Error conection";
+    }
+    else
+    {
+        std::cout << "Client connected! \n";
+        char msg[256] = "aaaaaaaaaaaaaaaaaaaaa";
+        send(newConnetion, msg, sizeof(msg), NULL);
+    }
+
+
+
     return 0;
 }
 
